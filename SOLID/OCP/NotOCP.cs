23d70509c8ffe151e15d19c266e6ccfcdf57ffcb
Hazal Yılmaz <hazal.yilmazx@hotmail.com>
2024-01-23 17:33:37 +0300

    added open closed principle

---
+++ SOLID/OCP/NotOCP.cs
@@ -0,0 +1,20 @@
+﻿namespace Solid.SOLID.OCP;
+
+public class NotOcp(string logType)
+{
+    public void Log(string message)
+    {
+        if (logType == "file")
+        {
+            // Dosyaya loglama işlemleri
+            Console.WriteLine("Logging to file: " + message);
+        }
+        else if (logType == "database")
+        {
+            // Veritabanına loglama işlemleri
+            Console.WriteLine("Logging to database: " + message);
+        }
+        // Her yeni loglama türü için, buraya yeni bir koşul eklememiz gerekir.
+        // Bu, OCP'ye aykırıdır çünkü yeni loglama türleri eklemek için mevcut kodu değiştirmemiz gerekmektedir.
+    }
+}
---
+++ SOLID/OCP/OCP.cs
@@ -0,0 +1,32 @@
+﻿namespace Solid.SOLID.OCP;
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class FileLogger : ILogger
+{
+    public void Log(string message)
+    {
+        // Dosya loglama işlemleri
+        Console.WriteLine($"Logging to file: {message}");
+    }
+}
+
+public class DatabaseLogger : ILogger
+{
+    public void Log(string message)
+    {
+        // Database loglama işlemleri
+        Console.WriteLine($"Logging to database: {message}");
+    }
+}
+
+public class LogManager(ILogger logger)
+{
+    public void Log(string message)
+    {
+        logger.Log(message);
+    }
+}
