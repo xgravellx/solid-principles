03654dfdc31bccdac4e19a678ef4cf2d615b679b
Hazal Yılmaz <hazal.yilmazx@hotmail.com>
2024-01-24 14:59:52 +0300

    added ISP

---
+++ SOLID/ISP/ISP.cs
@@ -0,0 +1,54 @@
+﻿namespace Solid.SOLID.ISP;
+
+public interface IWorking
+{
+    void Work();
+}
+
+public interface IEating
+{
+    void Eat();
+}
+
+public interface IGetPaid
+{
+    void GetPaid();
+}
+
+public interface IJoinMeet
+{
+    void JoinMeet();
+}
+
+public class SoftwareDevelopers : IWorking, IEating, IGetPaid, IJoinMeet
+{
+    public void Work()
+    {
+        Console.WriteLine("Çalış");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Yemek ye");
+    }
+    
+    public void GetPaid()
+    {
+        Console.WriteLine("Maaş al");
+    }
+    
+    public void JoinMeet()
+    {
+        Console.WriteLine("Toplantıya katıl");
+    }
+}
+
+public class Robots : IWorking
+{
+    public void Work()
+    {
+        Console.WriteLine("Çalış");
+    }
+}
+
+
---
+++ SOLID/ISP/NotISP.cs
@@ -0,0 +1,59 @@
+﻿namespace Solid.SOLID.ISP;
+
+public interface IWorker
+{
+    void Work();
+    void Eat();
+    void GetPaid();
+    void JoinMeet();
+}
+
+public class SoftwareDeveloper : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Çalış");
+    }
+    
+    public void Eat()
+    {
+        Console.WriteLine("Yemek ye");
+    }
+    
+    public void GetPaid()
+    {
+        Console.WriteLine("Maaş al");
+    }
+    
+    public void JoinMeet()
+    {
+        Console.WriteLine("Toplantıya katıl");
+    }
+}
+
+public class Robot : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Çalış");
+    }
+    
+    public void Eat()
+    {
+        // Robot yemek yemez, bu metod burada anlamsız.
+        throw new NotImplementedException();;
+    }
+    
+    public void GetPaid()
+    {
+        // Robot maaş almaz, bu metod burada anlamsız.
+        throw new NotImplementedException();
+    }
+    
+    public void JoinMeet()
+    {
+        // Robot toplantılara katılmaz, bu metod burada anlamsız.
+        throw new NotImplementedException();
+    }
+}
+
