d68daa3732ce34adb8e8e20a11983375ed0390ba
Hazal Yılmaz <hazal.yilmazx@hotmail.com>
2024-01-24 16:28:30 +0300

    added DIP

---
+++ SOLID/DIP/DIP.cs
@@ -0,0 +1,30 @@
+﻿public interface INotificationService
+{
+    void Send(string message);
+}
+
+public class EmailService : INotificationService
+{
+    public void Send(string message)
+    {
+        // Email gönderme işlemleri
+        Console.WriteLine("Email ile bildirim: " + message);
+    }
+}
+
+public class SmsService : INotificationService
+{
+    public void Send(string message)
+    {
+        // SMS gönderme işlemleri
+        Console.WriteLine("SMS ile bildirim: " + message);
+    }
+}
+
+public class User(INotificationService notificationService)
+{
+    public void Notify(string message)
+    {
+        notificationService.Send(message);
+    }
+}
---
+++ SOLID/DIP/NotDIP.cs
@@ -0,0 +1,20 @@
+﻿namespace Solid.SOLID.DIP;
+
+public class EmailService
+{
+    public static void SendEmail(string message)
+    {
+        // Email gönderme işlemleri
+        Console.WriteLine("Email gönderildi: " + message);
+    }
+}
+
+public class User
+{
+    private EmailService _emailService = new();
+    
+    public void Notify(string message)
+    {
+        EmailService.SendEmail(message);
+    }
+}
